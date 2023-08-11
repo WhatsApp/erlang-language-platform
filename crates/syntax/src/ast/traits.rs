8a77c8d282e8ad06cadc480c710ba466a84fdef6
Alan Zimmerman <alanzimm@meta.com>
2023-08-11 08:03:25 -0700

    BE: Change ast::HasLabel to return a Label
    Summary: Use a type alias to mark the String as a Label
    
    Reviewed By: robertoaloi
    
    Differential Revision: D48263727
    
    fbshipit-source-id: 232471aad58be1dc184fc2b8991502cedb0713ae

--- crates/syntax/src/ast/traits.rs
+++ crates/syntax/src/ast/traits.rs
@@ -15,34 +15,35 @@ use super::HasArity;
 use crate::ast;
 use crate::ast::support;
 use crate::ast::AstNode;
+use crate::label::Label;
 
 pub trait HasLabel: AstNode {
-    fn label(&self) -> Option<String> {
+    fn label(&self) -> Option<Label> {
         let name: ast::Name = support::child(self.syntax(), 0)?;
-        name.text()
+        name.text().map(|n| Label::new(n))
     }
 }
 
 impl HasLabel for ast::FunDecl {
-    fn label(&self) -> Option<String> {
+    fn label(&self) -> Option<Label> {
         let name = self.name()?.text()?;
         let arity = self.arity_value()?;
-        Some(format!("{}/{}", name, arity))
+        Some(Label::new(format!("{}/{}", name, arity)))
     }
 }
 impl HasLabel for ast::RecordDecl {}
 impl HasLabel for ast::TypeAlias {
-    fn label(&self) -> Option<String> {
-        self.name()?.name()?.text()
+    fn label(&self) -> Option<Label> {
+        self.name()?.name()?.text().map(|n| Label::new(n))
     }
 }
 impl HasLabel for ast::PpDefine {
-    fn label(&self) -> Option<String> {
+    fn label(&self) -> Option<Label> {
         let name = self.name()?.text()?;
         if let Some(arity) = self.arity_value() {
-            Some(format!("{}/{}", name, arity))
+            Some(Label::new(format!("{}/{}", name, arity)))
         } else {
-            Some(name)
+            Some(Label::new(name))
         }
     }
 }
