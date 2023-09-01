f179a1f0411be2071c7be029df4591dd655f2a4b
Jane Olszewska <threejane@meta.com>
2023-09-01 02:02:59 -0700

    add ignore_xref assist to deprecated function diagnostic
    Summary:
    The new assist will add an ignore attribute, compatible with rebar3 xref, to the module.
    
    Current limitations:
    
    1. since the linter does not check module attributes, it won't be silenced by the module ignore
    2. the assist does not group ignores, so there will be one ignore per MFA (this is valid, although perhaps noisy)
    3. the assist places them immediately after module attribute, which people might not like (but we can't depend on the order and placement of attributes otherwise)
    
    We should update the linter to check module attributes, so that it honours the existing ignores people have already added.
    
    Reviewed By: alanz
    
    Differential Revision: D48831227
    
    fbshipit-source-id: 1224827820f993cf5e30cd9aa53459c762a3f405

--- crates/ide/src/diagnostics/deprecated_function.rs
+++ crates/ide/src/diagnostics/deprecated_function.rs
@@ -17,13 +17,18 @@
 // This diagnostic does just that and shows the message at the call-site.
 // It also provides a mechanism to augment deprecation information with extra information, such as a URI.
 
+use elp_ide_assists::Assist;
 use elp_ide_db::elp_base_db::FileId;
+use elp_ide_db::source_change::SourceChange;
+use elp_syntax::AstNode;
 use hir::Expr;
 use hir::FunctionDef;
 use hir::Semantic;
 use hir::Strategy;
 use lazy_static::lazy_static;
+use text_edit::TextEdit;
 use text_edit::TextRange;
+use text_edit::TextSize;
 
 use super::Category;
 use super::Diagnostic;
@@ -32,6 +37,7 @@ use super::Severity;
 use crate::codemod_helpers::FunctionMatch;
 use crate::codemod_helpers::FunctionMatcher;
 // @fb-only: use crate::diagnostics;
+use crate::fix;
 
 #[derive(Debug, Clone)]
 pub struct DeprecationDetails {
@@ -122,7 +128,14 @@ pub(crate) fn check_function(
                             ctx.expr_id
                         };
                         if let Some(range) = def_fb.range_for_expr(sema.db, expr_id) {
+                            let range2 = range.clone();
                             let d = make_diagnostic(range, &target_def, details)
+                                .with_fixes(Some(vec![fix_xref_ignore(
+                                    sema,
+                                    def_fb.file_id(),
+                                    &target_def,
+                                    range2,
+                                )]))
                                 .with_ignore_fix(sema, def_fb.file_id());
                             diagnostics.push(d)
                         }
@@ -176,10 +189,48 @@ fn strip_quotes(s: &str) -> &str {
     s.strip_suffix(quote).unwrap_or(s)
 }
 
+fn fix_xref_ignore(
+    sema: &Semantic,
+    file_id: FileId,
+    def: &FunctionDef,
+    range: TextRange,
+) -> Assist {
+    let source = sema.parse(file_id).value;
+    let form_list = sema.form_list(file_id);
+
+    let offset = if let Some(module_attr) = form_list.module_attribute() {
+        let module_attr_range = module_attr.form_id.get(&source).syntax().text_range();
+        module_attr_range.end() + TextSize::from(1)
+    } else {
+        TextSize::from(0)
+    };
+
+    let module = sema.module_name(def.file.file_id).unwrap();
+
+    let text = format!(
+        "-ignore_xref([{{{}, {}, {}}}]).\n",
+        module.as_str(),
+        def.function.name.name(),
+        def.function.name.arity()
+    );
+    let mut edit_builder = TextEdit::builder();
+    edit_builder.insert(offset, text);
+    let edit = edit_builder.finish();
+    let source_change = SourceChange::from_text_edit(file_id, edit);
+
+    fix(
+        "xref_ignore",
+        "Add xref ignore for all calls to this function",
+        source_change,
+        range,
+    )
+}
+
 #[cfg(test)]
 mod tests {
 
     use crate::tests::check_diagnostics;
+    use crate::tests::check_fix;
 
     #[test]
     fn test_deprecated_function_local() {
@@ -252,6 +303,33 @@ mod tests {
 %%  ^^^^^^^^^^^^^^^^^ 💡 warning: Function 'not_ok_to_use/0' is deprecated.
 %%                  | Cause I said so.
             "#,
+        )
+    }
+
+    #[test]
+    fn test_xref_ignore_fix() {
+        check_fix(
+            r#"
+//- /src/b.erl
+-module(b).
+-export([not_ok_to_use/0]).
+-deprecated({not_ok_to_use, 0, "Cause I said so."}).
+not_ok_to_use() ->
+  ok.
+
+//- /src/a.erl
+-module(a).
+
+main() ->
+  b:no~t_ok_to_use().
+"#,
+            r#"
+-module(a).
+-ignore_xref([{b, not_ok_to_use, 0}]).
+
+main() ->
+  b:not_ok_to_use().
+"#,
         )
     }
 }
