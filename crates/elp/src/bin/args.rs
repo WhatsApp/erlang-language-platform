2391fdcd75d4aabc725d6244e12bcd5ebd9254cc
Alan Zimmerman <alanzimm@meta.com>
2023-12-15 09:51:32 -0800

    Add --include-tests option to elp lint as a no-op
    Summary:
    Add a command line option `elp lint` to include tests.
    This is currently not processed, but we will be able to update scripting to supply the parameter without breaking, so that when we honour it we preserve the existing behaviour.
    
    Reviewed By: jcpetruzza
    
    Differential Revision: D52207703
    
    fbshipit-source-id: f569fc82ee9707a11dc326a3103295409cd8805d

--- crates/elp/src/bin/args.rs
+++ crates/elp/src/bin/args.rs
@@ -209,6 +209,8 @@ pub struct Lint {
     pub include_ct_diagnostics: bool,
     /// Include EDoc diagnostics
     pub include_edoc_diagnostics: bool,
+    /// Also generate diagnostics for test files
+    pub include_tests: bool,
     /// If the diagnostic has an associated fix, apply it. The modified file will be in the --to directory, or original file if --in-place is set.
     pub apply_fix: bool,
     /// If applying fixes, apply any new ones that arise from the
