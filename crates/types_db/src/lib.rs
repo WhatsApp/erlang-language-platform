ed6162d30440662cb01243bac4b1752a34c855d1
Alan Zimmerman <alanzimm@meta.com>
2024-02-22 08:35:04 -0800

    Introduce elp_types_db crate
    Summary:
    This stack is about updating the `Semantic` API to be able to programme with inputs over and above ones from HIR. These, like Eqwalizer, require external processes and so cannot be used in the HIR crate directly, but are available in the IDE one, which depends on HIR.
    
    So we take a strategy of introducing a new crate, used in HIR and IDE, which just provides the types to be used in the API.  The provided API using these types returns `Option<_>` values, so will fail in HIR, but can be populated in IDE.
    
    It also allows us in future to provide implementations in `TestDB`, which can give us high-speed declarative tests of IDE features using a mock implementation of the data sources.
    
    This Diff simply does the housekeeping around introducing the crate.
    
    Reviewed By: michalmuskala, VLanvin
    
    Differential Revision: D53851948
    
    fbshipit-source-id: 75f2f2739ac37b7edad530eedce04ed811db2667

---
+++ crates/types_db/src/lib.rs
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under both the MIT license found in the
+ * LICENSE-MIT file in the root directory of this source tree and the Apache
+ * License, Version 2.0 found in the LICENSE-APACHE file in the root directory
+ * of this source tree.
+ */
+
+//! Types for use in HIR but which can also be produced by other code
+//! intelligence sources, such as Eqwalizer, OTP compiler,
+//! common_test.
+//!
+//! This crate carries the types only, so they can be used in
+//! hir::Semantic, without concern for the messy details of actually
+//! retrieving them.
+//!
+//! This also allows us to set up test fixtures to populate them
+//! without running heavyweight processes.
