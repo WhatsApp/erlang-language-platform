WhatsApp/erlang-language-platform

commit bf2fec560870080beaca2e823167b93f78869406

Completion: add common atoms to keywords
Summary: Add some common atoms to the keywords list so we get immediate, accurate completion for them.

Reviewed By: TheGeorge

Differential Revision: D55130986

fbshipit-source-id: 3fe5132a19a827955f9a90e15994ee9f1efcb817

crates/ide_completion/src/keywords.rs
@@ -44,7 +44,12 @@ lazy_static! {
         "rem",
         "try",
         "when",
-        "xor"
+        "xor",
+        // Add some common atoms too
+        "ok",
+        "undefined",
+        "true",
+        "false"
     ].iter().map(|label| Completion{ label: label.to_string(), kind: crate::Kind::Keyword, contents: Contents::SameAsLabel, position: None, sort_text: None, deprecated: false}).collect();
 }
 

commit 961cf29132e470b46080d1eeb7dc41741f343621

Fix tests after completion keywords update
Summary: D55130986 / bf2fec560870080beaca2e823167b93f78869406 broke the tests, fix them

Reviewed By: ilya-klyuchnikov

Differential Revision: D55184005

fbshipit-source-id: 566c22727528abb03af8e8d7046e42865db91c4b

crates/ide_completion/src/keywords.rs
@@ -109,16 +109,20 @@ mod test {
                 {label:cond, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:div, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:end, kind:Keyword, contents:SameAsLabel, position:None}
+                {label:false, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:fun, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:if, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:let, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:not, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:of, kind:Keyword, contents:SameAsLabel, position:None}
+                {label:ok, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:or, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:orelse, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:receive, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:rem, kind:Keyword, contents:SameAsLabel, position:None}
+                {label:true, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:try, kind:Keyword, contents:SameAsLabel, position:None}
+                {label:undefined, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:when, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:xor, kind:Keyword, contents:SameAsLabel, position:None}"#]],
         );
@@ -200,17 +204,21 @@ mod test {
                 {label:cond, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:div, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:end, kind:Keyword, contents:SameAsLabel, position:None}
+                {label:false, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:fun, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:if, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:let, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:main, kind:Module, contents:SameAsLabel, position:None}
                 {label:not, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:of, kind:Keyword, contents:SameAsLabel, position:None}
+                {label:ok, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:or, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:orelse, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:receive, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:rem, kind:Keyword, contents:SameAsLabel, position:None}
+                {label:true, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:try, kind:Keyword, contents:SameAsLabel, position:None}
+                {label:undefined, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:when, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:xor, kind:Keyword, contents:SameAsLabel, position:None}"#]],
         );
@@ -239,17 +247,21 @@ mod test {
                 {label:cond, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:div, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:end, kind:Keyword, contents:SameAsLabel, position:None}
+                {label:false, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:fun, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:if, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:let, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:main, kind:Module, contents:SameAsLabel, position:None}
                 {label:not, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:of, kind:Keyword, contents:SameAsLabel, position:None}
+                {label:ok, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:or, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:orelse, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:receive, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:rem, kind:Keyword, contents:SameAsLabel, position:None}
+                {label:true, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:try, kind:Keyword, contents:SameAsLabel, position:None}
+                {label:undefined, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:when, kind:Keyword, contents:SameAsLabel, position:None}
                 {label:xor, kind:Keyword, contents:SameAsLabel, position:None}"#]],
         );