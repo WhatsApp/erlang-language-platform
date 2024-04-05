6fd1f7b5e56dc3e95898395230956cf6c10fb506
Roman Perehonchuk <romanp@meta.com>
2024-04-05 05:37:42 -0700

    xref macro in pat
    Summary: as per title
    
    Reviewed By: alanz
    
    Differential Revision: D55129088
    
    fbshipit-source-id: c44ae7548384cf4364f7db790d8202e70fae4a09

--- crates/elp/src/bin/glean.rs
+++ crates/elp/src/bin/glean.rs
@@ -875,6 +875,18 @@ impl GleanIndexer {
                     }
                     acc
                 }
+                hir::AnyExpr::Pat(Pat::MacroCall {
+                    expansion: _,
+                    args: _,
+                    macro_def,
+                }) => {
+                    if let Some(def) = macro_def {
+                        if let Some(xref) = Self::resolve_macro_v2(&sema, def, &source_file, &ctx) {
+                            acc.push(xref);
+                        }
+                    }
+                    acc
+                }
                 _ => acc,
             },
             &mut |acc, _on, _form_id| acc,
@@ -1376,6 +1388,20 @@ mod tests {
         %%             ^^^ macro.hrl/macro/TAU/no_arity
             baz(N) -> ?MAX(N, 200).
         %%             ^^^ macro.erl/macro/MAX/2
+
+        "#;
+        xref_v2_check(&spec);
+    }
+
+    #[test]
+    fn xref_macro_in_pat_v2_test() {
+        let spec = r#"
+        //- /src/macro.erl
+            -module(macro).
+            -define(TAU, 6.28).
+
+            baz(?TAU) -> 1.
+        %%       ^^^ macro.erl/macro/TAU/no_arity
 
         "#;
         xref_v2_check(&spec);
