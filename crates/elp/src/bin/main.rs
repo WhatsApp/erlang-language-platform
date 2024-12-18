00bd9a8fa7c3f72646b269b38205a46bca23c85d
Roberto Aloi <robertoaloi@meta.com>
2024-12-18 02:40:45 -0800

    Add testcase showing incorrect exit code in case of no eqwalizer errors
    Summary:
    When the `--bail-on-error` flag is used for the `elp eqwalize` commands, the command exits with `101` even in absence of errors.
    This diff adds a new testcase showing the behaviour, before fixing it in the next diff.
    
    Reviewed By: Balajiganapathi
    
    Differential Revision: D67381790
    
    fbshipit-source-id: db13c95c55c5ac87cda915a20381866d1b705924

--- crates/elp/src/bin/main.rs
+++ crates/elp/src/bin/main.rs
@@ -1727,12 +1727,30 @@ mod tests {
 
     #[test_case(false ; "rebar")]
     #[test_case(true  ; "buck")]
-    fn eqwalize_all_bail_on_error(buck: bool) {
+    fn eqwalize_all_bail_on_error_failure(buck: bool) {
         if otp_supported_by_eqwalizer() {
             simple_snapshot_expect_error(
                 args_vec!["eqwalize-all", "--bail-on-error"],
                 "standard",
-                expect_file!("../resources/test/standard/eqwalize_all_bail_on_error.pretty"),
+                expect_file!(
+                    "../resources/test/standard/eqwalize_all_bail_on_error_failure.pretty"
+                ),
+                buck,
+                None,
+            );
+        }
+    }
+
+    #[test_case(false ; "rebar")]
+    #[test_case(true  ; "buck")]
+    fn eqwalize_all_bail_on_error_success(buck: bool) {
+        if otp_supported_by_eqwalizer() {
+            simple_snapshot_expect_error(
+                args_vec!["eqwalize", "--bail-on-error", "app_a_no_errors"],
+                "standard",
+                expect_file!(
+                    "../resources/test/standard/eqwalize_all_bail_on_error_success.pretty"
+                ),
                 buck,
                 None,
             );
