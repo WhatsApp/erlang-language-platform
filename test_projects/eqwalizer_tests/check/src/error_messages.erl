a5965ec59d72c09c3a4a0144d7c06a791cbbc821
Victor Lanvin <vlanvin@meta.com>
2025-02-21 07:00:48 -0800

    Snapshot -- imprecise error message with union of maps
    Summary: An example where error messages choose a bad candidate with a union of maps.
    
    Reviewed By: sopotc
    
    Differential Revision: D69980767
    
    fbshipit-source-id: 5b018000056cb33744b5cf4f859a41a1d1b0c519

---
+++ test_projects/eqwalizer_tests/check/src/error_messages.erl
@@ -0,0 +1,16 @@
+-module(error_messages).
+
+-compile([export_all, nowarn_export_all]).
+
+-type foo_map() :: #{
+    bar => atom(),
+    baz => integer(),
+    other => atom(),
+    other2 => atom(),
+    other3 => atom(),
+    other4 => atom(),
+    other5 => atom()}.
+
+-spec map_candidates(#{bar := atom(), baz := atom()}) ->
+    foo_map() | #{foo => atom()}.
+map_candidates(M) -> M.
