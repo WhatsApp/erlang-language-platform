b1167fbda3b5333f1dfb66aa19ae1e547c39c85a
Roberto Aloi <robertoaloi@meta.com>
2025-02-26 06:23:42 -0800

    Add plumbing for EDoc style comment auto-fix
    Summary:
    Add the necessary plumbing for an auto-fix for the EDoc diagnostics.
    Take the opportunity to refactor the convert the diagnostic/fix ids, labels and messages to constants.
    
    Reviewed By: alanz
    
    Differential Revision: D70235180
    
    fbshipit-source-id: 5053c7e23aaa60d28be86d986603629ac6bb5ea2

--- crates/ide/src/diagnostics/edoc.rs
+++ crates/ide/src/diagnostics/edoc.rs
@@ -9,14 +9,23 @@
 
 // Diagnostic: edoc
 
+use elp_ide_assists::Assist;
 use elp_ide_db::elp_base_db::FileId;
+use elp_ide_db::source_change::SourceChangeBuilder;
 use hir::Semantic;
+use text_edit::TextRange;
 
 use super::Diagnostic;
 use super::DiagnosticCode;
 use super::DiagnosticConditions;
 use super::DiagnosticDescriptor;
 use super::Severity;
+
+const DIAGNOSTIC_CODE: DiagnosticCode = DiagnosticCode::OldEdocSyntax;
+const DIAGNOSTIC_MESSAGE: &str = "EDoc style comments are deprecated. Please use Markdown instead.";
+const DIAGNOSTIC_SEVERITY: Severity = Severity::Warning;
+const CONVERT_FIX_ID: &str = "convert_to_markdown";
+const CONVERT_FIX_LABEL: &str = "Convert to Markdown";
 
 pub(crate) static DESCRIPTOR: DiagnosticDescriptor = DiagnosticDescriptor {
     conditions: DiagnosticConditions {
@@ -26,36 +35,55 @@ pub(crate) static DESCRIPTOR: DiagnosticDescriptor = DiagnosticDescriptor {
         default_disabled: true,
     },
     checker: &|diags, sema, file_id, _ext| {
-        edoc(diags, sema, file_id);
+        check(diags, sema, file_id);
     },
 };
 
-fn edoc(diagnostics: &mut Vec<Diagnostic>, sema: &Semantic, file_id: FileId) {
+fn check(diagnostics: &mut Vec<Diagnostic>, sema: &Semantic, file_id: FileId) {
     if let Some(comments) = sema.file_edoc_comments(file_id) {
         for (_form, header) in comments {
-            if let Some(doc) = header.doc().next() {
-                for range in doc.text_ranges() {
-                    diagnostics.push(
-                        Diagnostic::new(DiagnosticCode::OldEdocSyntax, "Old EDoc syntax", range)
-                            .with_severity(Severity::Warning),
-                    );
+            if let Some(doc_tag) = header.doc().next() {
+                for range in doc_tag.text_ranges() {
+                    diagnostics.push(old_edoc_syntax_diagnostic(file_id, range));
                 }
             }
         }
     }
 }
 
+fn old_edoc_syntax_diagnostic(file_id: FileId, range: TextRange) -> Diagnostic {
+    Diagnostic::new(DIAGNOSTIC_CODE, DIAGNOSTIC_MESSAGE, range)
+        .with_severity(DIAGNOSTIC_SEVERITY)
+        .with_fixes(Some(vec![fix_convert_to_markdown(file_id, range)]))
+}
+
+fn fix_convert_to_markdown(file_id: FileId, range: TextRange) -> Assist {
+    let mut builder = SourceChangeBuilder::new(file_id);
+    builder.replace(range, "todo");
+    let source_change = builder.finish();
+    crate::fix(CONVERT_FIX_ID, CONVERT_FIX_LABEL, source_change, range)
+}
+
 #[cfg(test)]
 mod tests {
 
     use elp_ide_db::DiagnosticCode;
+    use expect_test::expect;
+    use expect_test::Expect;
 
-    use crate::tests::check_diagnostics_with_config;
+    use crate::tests;
     use crate::DiagnosticsConfig;
 
+    fn config() -> DiagnosticsConfig {
+        DiagnosticsConfig::default().enable(DiagnosticCode::OldEdocSyntax)
+    }
+
     fn check_diagnostics(fixture: &str) {
-        let config = DiagnosticsConfig::default().enable(DiagnosticCode::OldEdocSyntax);
-        check_diagnostics_with_config(config, fixture);
+        tests::check_diagnostics_with_config(config(), fixture);
+    }
+
+    fn check_fix(fixture_before: &str, fixture_after: Expect) {
+        tests::check_fix_with_config(config(), fixture_before, fixture_after);
     }
 
     #[test]
@@ -67,11 +95,11 @@ mod tests {
     %% This is some license text.
     %%%-------------------------------------------------------------------
     %% @doc This is the module documentation.
-    %%<^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ warning: Old EDoc syntax
+    %%<^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 💡 warning: EDoc style comments are deprecated. Please use Markdown instead.
     %%      With some more text.
-    %%<^^^^^^^^^^^^^^^^^^^^^^^^^ warning: Old EDoc syntax
+    %%<^^^^^^^^^^^^^^^^^^^^^^^^^ 💡 warning: EDoc style comments are deprecated. Please use Markdown instead.
     %%      And some more lines.
-    %%<^^^^^^^^^^^^^^^^^^^^^^^^^ warning: Old EDoc syntax
+    %%<^^^^^^^^^^^^^^^^^^^^^^^^^ 💡 warning: EDoc style comments are deprecated. Please use Markdown instead.
     %% @end
     %%%-------------------------------------------------------------------
     %%% % @format
@@ -91,12 +119,34 @@ mod tests {
             r#"
     -module(main).
     %% @doc This is the main function documentation.
-    %%<^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ warning: Old EDoc syntax
+    %%<^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 💡 warning: EDoc style comments are deprecated. Please use Markdown instead.
     main() ->
       dep().
 
     dep() -> ok.
         "#,
+        )
+    }
+
+    #[test]
+    fn test_function_doc_fix() {
+        check_fix(
+            r#"
+    -module(main).
+    %% @doc This is the main f~unction documentation.
+    main() ->
+      dep().
+
+    dep() -> ok.
+        "#,
+            expect![[r#"
+    -module(main).
+    todo
+    main() ->
+      dep().
+
+    dep() -> ok.
+        "#]],
         )
     }
 }
