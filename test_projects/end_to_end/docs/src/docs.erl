dac0ab0eef9b2f58aa547e060eaf11b3589325b6
Roberto Aloi <robertoaloi@meta.com>
2025-09-30 04:24:23 -0700

    Fix broken VSCodeErlangOpenDocs E2E test
    Summary:
    In D82544032 ELP was modified to only return doc links for functions containing an actual documentation.
    While there may be other issues affecting the VSCodeErlangOpenDocs E2E test, this is a requirement for the test to pass.
    
    Reviewed By: TheGeorge
    
    Differential Revision: D83559010
    
    fbshipit-source-id: 2cda6051d38a486ef9854a992877e7113bbe6370

--- test_projects/end_to_end/docs/src/docs.erl
+++ test_projects/end_to_end/docs/src/docs.erl
@@ -2,6 +2,9 @@
 -compile(nowarn_missing_spec).
 -export([my_function/0, my_other_function/0]).
 
+-doc """
+This is the doc for my_function
+""".
 my_function() -> ok.
 
 my_other_function() -> my_function().
