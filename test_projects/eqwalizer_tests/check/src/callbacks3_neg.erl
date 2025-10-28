a269e114195080fe02d97580536c2af3925180aa
Victor Lanvin <vlanvin@meta.com>
2025-10-28 09:52:56 -0700

    Fix CI for OTP 28
    Summary: Snapshot `callbacks3_neg` is also OTP version dependent, fix it.
    
    Reviewed By: michalmuskala
    
    Differential Revision: D85675523
    
    fbshipit-source-id: dfabda187190b669ef509c967fe3723de25d2890

--- test_projects/eqwalizer_tests/check/src/callbacks3_neg.erl
+++ test_projects/eqwalizer_tests/check/src/callbacks3_neg.erl
@@ -3,7 +3,7 @@
 %%% This source code is licensed under the Apache 2.0 license found in
 %%% the LICENSE file in the root directory of this source tree.
 
--module(callbacks3_neg).
+-module(callbacks3_neg). % @OTPVersionDependent
 -export([
     init/1,
     handle_call/3,
