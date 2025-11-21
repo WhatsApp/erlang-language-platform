0c2b43eb27b5c9a476abb83296f4ef4ad4b17d88
Alan Zimmerman <alanzimm@meta.com>
2025-11-21 10:19:14 -0800

    ssr: add test explicitly using colour output
    Summary: As title
    
    Reviewed By: TD5
    
    Differential Revision: D87651912
    
    fbshipit-source-id: 347be29dbb6f0dd71396dbb404b2ac98a9dbabc0

--- crates/elp/src/bin/main.rs
+++ crates/elp/src/bin/main.rs
@@ -2059,6 +2059,23 @@ mod tests {
     }
 
     #[test]
+    fn lint_ssr_with_context_and_separator_color() {
+        simple_snapshot(
+            args_vec![
+                "ssr",
+                "--context", "2",
+                "--group-separator", "====",
+                "--colour", "always"
+                "{_@A, _@B}",
+            ],
+            "linter",
+            expect_file!("../resources/test/linter/ssr_context_separator_color.stdout"),
+            true,
+            None,
+        )
+    }
+
+    #[test]
     fn lint_ssr_as_cli_arg_multiple_patterns() {
         simple_snapshot(
             args_vec!["ssr", "3" "{4}",],
