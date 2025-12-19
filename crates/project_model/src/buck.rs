cd0d3cb5d26883c205e02b6077f1faa4e6515d2a
Roberto Aloi <robertoaloi@meta.com>
2025-12-19 14:07:40 +0100

    Enable Buck2 tests

--- crates/project_model/src/buck.rs
+++ crates/project_model/src/buck.rs
@@ -1643,9 +1643,7 @@ mod tests {
         assert_eq!(expected, actual)
     }
 
-    // TODO: enable when buck is properly set up on github project
-    // @fb-only: const BUCK_TESTS_ENABLED: bool = true;
-    const BUCK_TESTS_ENABLED: bool = false; // @oss-only
+    const BUCK_TESTS_ENABLED: bool = true;
 
     #[track_caller]
     fn check_buck_bxl_query(build_generated: bool, expect: Expect) {
