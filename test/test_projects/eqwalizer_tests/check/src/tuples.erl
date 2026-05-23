c3366bafc63dc3057c3c77419fe14c27f625b46e
Ilya Klyuchnikov <ilyaklyuchnikov@meta.com>
2026-05-23 03:18:10 -0700

    tests showing inconsistent handling of tuple() type
    Summary:
    New tests showing that the tuple() internals of eqwalizer behave wrongly (they behave mostly as expected though, - this is why we had this problem for while).
    
    The problem: we try to treat AnyTupleType internally both as
    - `{} | {term()} | {term(), term(), term()} | ...` and
    - some form of `dynamic() & {} | {term()} | {term(), term(), term()} | ...`
    
    in the next diffs we explore different ways of fixing it holistically
    
    ___
    
    Differential Revision: D106178790
    
    fbshipit-source-id: cf8c65d0060dfcb137db77f7a4549b8793053221

---
+++ test/test_projects/eqwalizer_tests/check/src/tuples.erl
@@ -0,0 +1,23 @@
+%%% Copyright (c) Meta Platforms, Inc. and affiliates. All rights reserved.
+%%%
+%%% This source code is licensed under the Apache 2.0 license found in
+%%% the LICENSE file in the root directory of this source tree.
+
+-module(tuples).
+
+-compile([export_all, nowarn_export_all]).
+
+-spec test1(tuple()) -> integer().
+test1(Tuple) ->
+    case Tuple of
+        {_} -> ok
+    end,
+    eqwalizer:reveal_type(Tuple),
+    {Z} = Tuple,
+    Z.
+
+-spec test2_neg(term()) -> {atom(), binary()}.
+test2_neg(Term) ->
+    case Term of
+        T when is_tuple(T) -> T
+    end.
