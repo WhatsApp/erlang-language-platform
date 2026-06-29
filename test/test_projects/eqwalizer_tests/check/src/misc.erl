f06fdd7fc223f23c6b8f163b104da66ddeec9b57
Roberto Aloi <robertoaloi@meta.com>
2026-06-29 13:20:04 +0200

    [ELP][OTP 29] Update elp_lint for to include support unsafe function checks, update tests

--- test/test_projects/eqwalizer_tests/check/src/misc.erl
+++ test/test_projects/eqwalizer_tests/check/src/misc.erl
@@ -3,7 +3,7 @@
 %%% This source code is licensed under the Apache 2.0 license found in
 %%% the LICENSE file in the root directory of this source tree.
 
--module(misc).
+-module(misc). % @OTP29Only
 
 %% The majority of these tests are just
 %% to get some code coverage.
--- test/test_projects/eqwalizer_tests/check/src/otp_opaques.erl
+++ test/test_projects/eqwalizer_tests/check/src/otp_opaques.erl
@@ -3,7 +3,7 @@
 %%% This source code is licensed under the Apache 2.0 license found in
 %%% the LICENSE file in the root directory of this source tree.
 
--module(otp_opaques).
+-module(otp_opaques). % @OTP29Only
 
 -compile([export_all, nowarn_export_all]).
 
